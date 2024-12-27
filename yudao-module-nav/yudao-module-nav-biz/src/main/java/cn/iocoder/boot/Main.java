fbc58d8e8647ccc70d307f08552c0dd0339d471d
cap <pujunhong3@gmail.com>
2024-12-27 17:56:21 +0800

    扩展工程

---
+++ yudao-module-nav/yudao-module-nav-biz/src/main/java/cn/iocoder/boot/Main.java
@@ -0,0 +1,13 @@
+package cn.iocoder.boot;
+
+/**
+ * @version 1.0
+ * @Author pujh
+ * @Date 2024/12/27 10:04
+ * @description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
